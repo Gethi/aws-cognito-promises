dc3cc76f0380fe062b58454b2b04dacb836e1859
Gethi <bigard.antoine@gmail.com>
2019-02-02 15:09:27 +0100

    fix not auth + bump v1.6.9

--- src/config.js
+++ src/config.js
@@ -1,17 +1,21 @@
 import Amplify, { Auth } from 'aws-amplify';
 
-let appConfig
-let userPool
-let currentUser
+let appConfig = null;
+let userPool = null;
+let currentUser = null;
 
 export const set = async ampConfig => {
   appConfig = ampConfig;
 
   Amplify.configure(ampConfig);
-  const cognitoUser = await Auth.currentAuthenticatedUser({
-      bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  });
-  userPool = cognitoUser.pool;
+  try {
+    const cognitoUser = await Auth.currentAuthenticatedUser({
+        bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    });
+    userPool = cognitoUser.pool;
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const get = () => {
@@ -23,9 +27,18 @@ export const getUserPool = () => {
 }
 
 export const getUser = async () => {
-  const cognitoUser = await Auth.currentAuthenticatedUser({
-      bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  });
+  let cognitoUser = null;
+  try {
+    cognitoUser = await Auth.currentAuthenticatedUser({
+        bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    });
+    if(!userPool) {
+      userPool = cognitoUser.pool;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+
   return cognitoUser;
 }
 
